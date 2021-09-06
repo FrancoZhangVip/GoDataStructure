6e593a7e3700aa8f95b2e22647d290f852cd86ec
Franco <15129655781@163.com>
2021-09-06 23:33:43 +0800

    无重复字符的最长子串 add assert to testify

--- algorithm_init/Unique_longest_string_test.go
+++ algorithm_init/Unique_longest_string_test.go
@@ -1,26 +1,36 @@
 package algorithm_init
 
-import "testing"
+import (
+	"github.com/hedzr/assert"
+	"testing"
+)
 
 func TestLengthOfLongestSubstring(t *testing.T) {
 	s := ""
 	println("Test 1 is: ", LengthOfLongestSubstring(s)) //0
+	assert.Equal(t, 0, LengthOfLongestSubstring(s))
 
 	s = "abcabcbb"
 	println("Test 2 is: ", LengthOfLongestSubstring(s)) //3
+	assert.Equal(t, 3, LengthOfLongestSubstring(s))
 
 	s = "bbbbb"
 	println("Test 3 is: ", LengthOfLongestSubstring(s)) //1
+	assert.Equal(t, 1, LengthOfLongestSubstring(s))
 
 	s = "pwwkew"
 	println("Test 4 is: ", LengthOfLongestSubstring(s)) //3
+	assert.Equal(t, 3, LengthOfLongestSubstring(s))
 
 	s = " "
 	println("Test 5 is: ", LengthOfLongestSubstring(s)) //1
+	assert.Equal(t, 1, LengthOfLongestSubstring(s))
 
 	s = "aa"
 	println("Test 6 is: ", LengthOfLongestSubstring(s)) //1
+	assert.Equal(t, 1, LengthOfLongestSubstring(s))
 
 	s = "aab"
 	println("Test 7 is: ", LengthOfLongestSubstring(s)) //2
+	assert.Equal(t, 2, LengthOfLongestSubstring(s))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module GoDataStructure
 
 go 1.14
+
+require github.com/hedzr/assert v0.1.3
