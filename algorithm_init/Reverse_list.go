e2d6a214652484d1d4128f93b40b8c8ffbb8c4aa
Franco <15129655781@163.com>
2021-09-08 01:32:43 +0800

    add best answer to Reverse_list.go

--- algorithm_init/Reverse_list.go
+++ algorithm_init/Reverse_list.go
@@ -57,3 +57,33 @@ func ReverseList(head *ListNode) *ListNode {
 	head.Next = nil
 	return start
 }
+
+/*
+
+//递归
+func reverseList(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil {
+        return head
+    }
+    newHead := reverseList(head.Next)
+    head.Next.Next = head
+    head.Next = nil
+    return newHead
+}
+
+
+//循环
+func reverseList(head *ListNode) *ListNode {
+    var prev *ListNode
+    curr := head
+    for curr != nil {
+        next := curr.Next
+        curr.Next = prev
+        prev = curr
+        curr = next
+    }
+    return prev
+}
+
+
+*/
