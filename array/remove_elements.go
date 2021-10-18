c57829cd8a339e33dccc54fb3b0cbc77c4919912
Franco <15129655781@163.com>
2021-10-19 00:35:56 +0800

    add answer

--- array/remove_elements.go
+++ array/remove_elements.go
@@ -40,3 +40,17 @@ func RemoveElement(nums []int, val int) int {
 
 	return len(nums) - counter
 }
+
+/*
+func removeElement(nums []int, val int) int {
+    left := 0
+    for _, v := range nums { // v 即 nums[right]
+        if v != val {
+            nums[left] = v
+            left++
+        }
+    }
+    return left
+}
+
+*/
