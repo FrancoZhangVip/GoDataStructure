e5f9cebefefc0badf85a8c4703b5b43b76d6317e
Franco <15129655781@163.com>
2021-12-02 01:57:17 +0800

    add reverse_left_words.go

---
+++ chars/reverse_left_words.go
@@ -0,0 +1,34 @@
+package chars
+
+/*
+字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，
+该函数将返回左旋转两位得到的结果"cdefgab"。
+
+示例 1：
+输入: s = "abcdefg", k = 2
+输出: "cdefgab"
+
+示例 2：
+输入: s = "lrloseumgh", k = 6
+输出: "umghlrlose"
+
+限制：
+1 <= k < s.length <= 10000
+*/
+
+func ReverseLeftWords(s string, n int) string {
+	data := []byte(s)
+	for i := 0; i < n/2; i++ {
+		data[i], data[n-1-i] = data[n-1-i], data[i]
+	}
+
+	for i := n; i < (len(data)+n)/2; i++ {
+		data[i], data[len(data)-1-(i-n)] = data[len(data)-1-(i-n)], data[i]
+	}
+
+	for i := 0; i < len(data)/2; i++ {
+		data[i], data[len(data)-1-i] = data[len(data)-1-i], data[i]
+	}
+
+	return string(data)
+}
---
+++ chars/reverse_left_words_test.go
@@ -0,0 +1,18 @@
+package chars
+
+import (
+	"github.com/hedzr/assert"
+	"testing"
+)
+
+func TestReverseLeftWords(t *testing.T) {
+	s := "abcdefg"
+	k := 2
+	assert.Equal(t, "cdefgab", ReverseLeftWords(s, k))
+}
+
+func TestReverseLeftWords2(t *testing.T) {
+	s := "lrloseumgh"
+	k := 6
+	assert.Equal(t, "umghlrlose", ReverseLeftWords(s, k))
+}
